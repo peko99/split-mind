07b53db44f59e74309972a2e71f09914b748230a
Marin Pejčin <marin.pejcin@cellusys.com>
2021-10-23 14:15:20 +0200

    At sign hello world

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
+import './screens/login_screen.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '@Protocol Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: LoginScreen.id,
+      routes: <String, Widget Function(BuildContext)>{
+        LoginScreen.id: (BuildContext context) => LoginScreen(),
+        HomeScreen.id: (BuildContext context) => HomeScreen(),
+      },
+    );
+  }
+}
